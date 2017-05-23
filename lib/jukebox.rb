7a04daa3cfd71d398c025c6e17e7e4159c6dcaac
Diljeet <jeetds1994@gmail.com>
2017-05-22 23:47:28 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,73 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each do |x|
+    puts x
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  if songs.include?(user_input)
+    puts "Playing #{user_input}"
+  elsif user_input == "exit"
+    exit_jukebox
+  else
+    convert2Numb = user_input.to_i
+    if convert2Numb == 0
+      puts "Invalid input, please try again"
+    elsif convert2Numb <= songs.length
+      puts "Playing #{songs[convert2Numb - 1]}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_input = gets.chomp
+  answer = ""
+
+  until answer == "exit"
+    if user_input == "exit"
+      exit_jukebox
+      answer = user_input
+    elsif answer == "play"
+      play(songs)
+      puts "Please enter a command:"
+      user_input = gets.chomp
+      answer = user_input
+    elsif user_input == "help"
+      help
+      puts "Please enter a command:"
+      user_input = gets.chomp
+      answer = user_input
+    elsif user_input == "list"
+      list(songs)
+      puts "Please enter a command:"
+      user_input = gets.chomp
+      answer = user_input
+    end
+
+  end
+
+end
